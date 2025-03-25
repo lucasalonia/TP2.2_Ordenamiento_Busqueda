79c21e1e8c07930c2817ad89eee612ed7afa848a
lucasalonia <salonialuca97@gmail.com>
2025-03-25 18:55:17 -0300

    Initial commit

---
+++ src/tp2/pkg2_ordenamiento_busqueda/Pais.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2.pkg2_ordenamiento_busqueda;
+
+/**
+ *
+ * @author salon
+ */
+public class Pais implements Comparable<Pais>{
+    private String nombre;
+    private Integer medallasOro;
+    private Integer medallasPlata;
+    private Integer medallasBronce;
+    private int ranking;
+
+    public Pais(String nombre, Integer medallasOro, Integer medallasPlata, Integer medallasBronce) {
+        this.nombre = nombre;
+        this.medallasOro = medallasOro;
+        this.medallasPlata = medallasPlata;
+        this.medallasBronce = medallasBronce;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getMedallasOro() {
+        return medallasOro;
+    }
+
+    public Integer getMedallasPlata() {
+        return medallasPlata;
+    }
+
+    public Integer getMedallasBronce() {
+        return medallasBronce;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+     
+    
+    @Override
+    public int compareTo(Pais otroPais) {
+        //Devuelve 0 cuando son iguales. Numero positivo cuando era mayor y negativo si es menor
+        int oro = medallasOro.compareTo(otroPais.medallasOro);
+        int plata = medallasPlata.compareTo(otroPais.medallasPlata);
+        int bronce = medallasBronce.compareTo(otroPais.medallasBronce);
+        int pais = this.nombre.compareTo(otroPais.nombre);
+        
+        if (oro!=0) {
+            return oro;
+        }
+        if (plata!=0) {
+            return plata;
+        }
+        if (bronce!=0) {
+            return bronce;
+        }
+        
+       return pais; 
+    }
+    
+    
+}
---
+++ src/tp2/pkg2_ordenamiento_busqueda/TP22_Ordenamiento_Busqueda.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2.pkg2_ordenamiento_busqueda;
+
+import java.lang.reflect.Array;
+
+/**
+ *
+ * @author salon
+ */
+public class TP22_Ordenamiento_Busqueda {
+
+    /**
+     * @param arregloPais
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pais[] JJOO = new Pais[3];
+        JJOO[0] = new Pais("Brasil", 2,5,1);
+        JJOO[1] = new Pais("Argentina", 3,2,3);
+        JJOO[2] = new Pais("Nicaragua", 4,1,2);
+        
+    }
+    
+}
